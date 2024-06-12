b821ffe12da7b9d08635899526bc5f57049bdb26
millena26 <149429116+millena26@users.noreply.github.com>
2024-06-12 11:24:26 -0300

    salvar

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+function setup() {
+  createCanvas(400, 400);
+   background(color(100,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 0;
+  posicaoVertical = 200;
+}
+
+
+function draw() {
+ 
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,50);
+ 
+ posicaoHorizontal+=random(0 , 3);
+ posicaoVertical+=random(-3 , 3);
+ 
+  if(mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+   
+}
